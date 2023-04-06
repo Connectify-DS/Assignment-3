3cd450505debe0b513f62602fb62f2b21912ee4d
ShrinivasSK <56220091+ShrinivasSK@users.noreply.github.com>
2023-04-06 19:01:12 +0530

    Added Part 1 Code

---
+++ atm_raft.py
@@ -0,0 +1,92 @@
+import sys
+from pysyncobj import SyncObj, replicated_sync
+
+class ATMNetwork(SyncObj):
+    def __init__(self, selfNode, partnerNodes):
+        super(ATMNetwork, self).__init__(selfNode, partnerNodes)
+        # This dictionary will be used to store account balances
+        self.account_balances = {'Ishan': 1000, 
+                                 'Shrinivas': 1000,
+                                 'Mayank': 1000,
+                                 'Shashwat': 1000,
+                                 'Yashica': 1000
+                            }
+
+    # Withdrawal endpoint
+    @replicated_sync
+    def withdrawal(self, account, amount):
+        if account in self.account_balances and amount <= self.account_balances[account]:
+            self.account_balances[account] -= amount
+            return 'Withdrawal successful'
+        else:
+            return 'Withdrawal failed'
+
+    # Deposit endpoint
+    @replicated_sync
+    def deposit(self, account, amount):
+        if account in self.account_balances:
+            self.account_balances[account] += amount
+            return 'Deposit successful'
+        else:
+            return 'Deposit failed'
+
+    # Balance enquiry endpoint
+    def balance(self, account):
+        if account in self.account_balances:
+            return self.account_balances[account]
+        else:
+            return 'Account not found'
+
+    # Transfer endpoint
+    @replicated_sync
+    def transfer(self, account1, account2, amount):
+        if account1 in self.account_balances and account2 in self.account_balances and amount <= self.account_balances[account1]:
+            self.account_balances[account1] -= amount
+            self.account_balances[account2] += amount
+            return 'Transfer successful'
+        else:
+            return 'Transfer failed'
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print('Usage: %s self_port partner1_port partner2_port ...' % sys.argv[0])
+        sys.exit(-1)
+
+    port = int(sys.argv[1]) # The address of the current node
+    partners = ['localhost:%d' % int(p) for p in sys.argv[2:]] # The addresses of the partner nodes
+
+    network = ATMNetwork('localhost:%d' % port, partners)
+
+    # Interactive mode for user input
+    while True:
+        print('ATM network commands:')
+        print('1. Withdrawal')
+        print('2. Deposit')
+        print('3. Balance enquiry')
+        print('4. Transfer')
+        print('5. Exit')
+        command = input('Enter command number: ')
+        if command == '1':
+            account = input('Enter account name: ')
+            amount = int(input('Enter withdrawal amount: '))
+            result = network.withdrawal(account, amount)
+            print(result)
+        elif command == '2':
+            account = input('Enter account name: ')
+            amount = int(input('Enter deposit amount: '))
+            result = network.deposit(account, amount)
+            print(result)
+        elif command == '3':
+            account = input('Enter account name: ')
+            result = network.balance(account)
+            print(result)
+        elif command == '4':
+            account1 = input('Enter account name to transfer from: ')
+            account2 = input('Enter account name to transfer to: ')
+            amount = int(input('Enter transfer amount: '))
+            result = network.transfer(account1, account2, amount)
+            print(result)
+        elif command == '5':
+            break
+        else:
+            print('Invalid command number')
